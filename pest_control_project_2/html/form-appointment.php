b8676f39b28c91edda428c91f8d94c3452854cb0
emirsultan1 <162753105+emirsultan1@users.noreply.github.com>
2026-02-07 21:25:20 +0200

    first_commit

---
+++ pest_control_project_2/html/form-appointment.php
@@ -0,0 +1,76 @@
+<?php
+
+	$errorMSG = "";
+
+	// FULLNAME
+	if (empty($_POST["name"])) {
+		$errorMSG = "Full Name is required. ";
+	} else {
+		$name = $_POST["name"];
+	}
+
+	// EMAIL
+	if (empty($_POST["email"])) {
+		$errorMSG .= "Email is required. ";
+	} else {
+		$email = $_POST["email"];
+	}
+
+	// PHONE
+	if (empty($_POST["phone"])) {
+		$errorMSG .= "Phone is required. ";
+	} else {
+		$phone = $_POST["phone"];
+	}
+
+	// DATE
+	if (empty($_POST["date"])) {
+		$errorMSG .= "Date is required. ";
+	} else {
+		$date = $_POST["date"];
+	}
+
+	// SERVICES
+	if (empty($_POST["services"])) {
+		$errorMSG = "Services is required. ";
+	} else {
+		$services = $_POST["services"];
+	}
+
+	$subject ='Book Appointment from site';
+
+	$EmailTo = "info@yourdomain.com"; // Replace with your email.
+
+	// prepare email body text
+	$Body = "";
+	$Body .= "name: ";
+	$Body .= $name;
+	$Body .= "\n";
+	$Body .= "Email: ";
+	$Body .= $email;
+	$Body .= "\n";
+	$Body .= "Phone: ";
+	$Body .= $phone;
+	$Body .= "\n";
+	$Body .= "Date: ";
+	$Body .= $date;
+	$Body .= "\n";
+	$Body .= "services: ";
+	$Body .= $services;
+	$Body .= "\n";
+
+	// send email
+	$success = @mail($EmailTo, $subject, $Body, "From:".$email);
+
+	// redirect to success page
+	if ($success && $errorMSG == ""){
+	   echo "success";
+	}else{
+		if($errorMSG == ""){
+			echo "Something went wrong :(";
+		} else {
+			echo $errorMSG;
+		}
+	}
+
+?>
